425df051fb9643210f0e5ad020e373d5782a6353
Parag Agarwal <parag.airun@gmail.com>
2026-07-20 13:16:59 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import : The term 'import' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is correct
-and try again.
-At line:1 char:1
-+ import { createClient } from '@supabase/supabase-js';
-+ ~~~~~~
-    + CategoryInfo          : ObjectNotFound: (import:String) [], CommandNotFoundException
-    + FullyQualifiedErrorId : CommandNotFoundException
+export const supabase = createClient(
+  'https://kfjwztbfcmgzryzmnzni.supabase.co',
+  'sb_publishable_e2SAmaMFieRYxkc5jH_RUg_7zcyyDa9'
+);
